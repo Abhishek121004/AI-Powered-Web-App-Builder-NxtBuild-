16efc4fc4739a80d02dd1e3af216d6fbf13aa83a
Abhishek121004 <abhishekk121004@gmail.com>
2026-03-28 14:15:25 +0530

    Fixed server start issue

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -2,14 +2,14 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI;
+    const mongoDBURI = process.env.MONGODB_URI;
 
-    if (!mongoURI) {
+    if (!mongoDBURI) {
       throw new Error('MONGODB_URI is not defined in your .env file');
     }
 
     // Mongoose 9.x handles connection options automatically
-    const conn = await mongoose.connect(mongoURI);
+    const conn = await mongoose.connect(mongoDBURI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
